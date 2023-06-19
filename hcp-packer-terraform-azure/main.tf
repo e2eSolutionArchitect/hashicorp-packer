a8ff1e2144832776dc2debf00b2faf6dc3e0756f
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2023-06-19 14:02:49 -0400

    tf spec updated

--- hcp-packer-terraform-azure/main.tf
+++ hcp-packer-terraform-azure/main.tf
@@ -39,8 +39,32 @@ resource "azurerm_network_security_group" "az_nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
+    name                       = "HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
     name                       = "SSH"
-    priority                   = 1001
+    priority                   = 101
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
